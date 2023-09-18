bba42ab1ad6337b5fba76c06e224808f0458d977
WeissCurry <Maulanasykari@gmail.com>
2023-09-18 21:41:42 +0700

    test2

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # DMS_Project
+>bismillah bisa!
+
---
+++ server/app.js
@@ -0,0 +1,9 @@
+
+let colors = require("colors")
+let result = colors.bgCyan("Halo")
+
+let namaUcup = "haiiiii"
+console.log(result)
+console.log(namaUcup.bgYellow);
+
+
