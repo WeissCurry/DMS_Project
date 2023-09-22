3d8cdd9fb4487be1ef673c6599aad7057508470e
WeissCurry <Maulanasykari@gmail.com>
2023-09-22 08:39:26 +0700

    try

--- server/app.js
+++ server/app.js
@@ -5,5 +5,17 @@ let result = colors.bgCyan("Halo")
 let namaUcup = "haiiiii"
 console.log(result)
 console.log(namaUcup.bgYellow);
+console.log(namaUcup.bold.red);
 
+console.log("");
+console.log("INI TULISAN NGEGGAS".bold.red);
 
+console.log("ucup surucup".strikethrough.grey);
+
+const ereyY = ["ucup", "ucup", "ucup", "ucup", "ucup"]
+
+for (let i = 0; i < ereyY.length; i++) {
+    console.log(ereyY[i].bold);
+    console.log(ereyY[i].strip);
+    console.log(ereyY[i].stripColors.bgWhite);
+}
